6222e21becaec4ce41e092c93f8005864da5fd2c
mennaAltear78 <mennaaltear@gmail.com>
2026-08-22 14:29:47 +0300

    fix schema spelling import

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174",
-        "https://Fcustomer-segmentation12menna"    ],
+        "https://fcustomer-segmentation12menna"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
